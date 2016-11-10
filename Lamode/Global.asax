362912c276ef0b20292b54bc0fa38bc8dfea5ce0
parsa <f_lamee2002@yahoo.com>
2016-11-10 14:53:30 -0800

    first commit

---
+++ Lamode/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Lamode.MvcApplication" Language="C#" %>
---
+++ Lamode/Startup1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.AspNet.Identity;
+
+[assembly: OwinStartup(typeof(Lamode.Startup1))]
+
+namespace Lamode
+{
+    public class Startup1
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Home/Index")
+            });
+        }
+    }
+}
